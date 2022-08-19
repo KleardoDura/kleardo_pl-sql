423c0b56ffca5ba1be8c8a6eecbffae010877d82
FACILIZATION\kleardo.dura <kleardodura13@gmail.com>
2022-08-19 12:21:24 +0200

    no message

--- ushtrimi 3/f_kleardo_min_max_year.sql
+++ ushtrimi 3/f_kleardo_min_max_year.sql
@@ -5,8 +5,19 @@ as
   i boolean;
 begin
   
+begin
   select max(year)  into max_year from STTMS_LCL_HOLIDAY;
+ exception 
+    when others then
+     dbms_output.put_line('ERROR');
+end;
+
+begin
   select min(year)  into  min_year from STTMS_LCL_HOLIDAY;
+ exception 
+    when others then
+     dbms_output.put_line('ERROR');
+end;
   
   if( l_year>=min_year and l_year<=max_year) then
      return true;
